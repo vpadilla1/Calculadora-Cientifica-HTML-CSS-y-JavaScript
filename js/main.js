554d82c3683e1f905ce4cd627044f2480f0d8a25
Victor-Padilla <Evictor@gmail.com>
2020-10-04 19:53:24 -0500

    inicio de javaScrip

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-consolo.log("hola mundo")
+var vaiMudar = false, cont, valor, historico = Array(), memoria = Array(), fe = false, hyp=false; deg="deg";
+
+function atualiza() {
+    $(this).css('background-color', 'rgb(192,192,192)');
+    valor = $('#valor').text();
+    valor = valor.replace(',','.');
+    setTimeout(function(){
+        $('.col').css('background-color', 'rgb(240, 240, 240)');
+        $('.numeros').css('background-color', 'rgb(250, 250, 250)');
+    },100);
+    $('.col').hover(function(){
+        $(this).css('background-color', 'rgb(216,216,216)');
+    }, function(){
+        $(this).css('background-color', 'rgb(240,240,240)');
+    });
+    $('.numeros').hover(function(){
+        $(this).css('background-color', 'rgb(216,216,216)');
+    }, function(){
+        $(this).css('background-color', 'rgb(250,250,250)');
+    });
+}
